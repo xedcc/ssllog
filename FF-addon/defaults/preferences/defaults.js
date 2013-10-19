a6b4ae9cc2c3878a89b39ea5c9bb7b91e439f430
default2 <default2@default.(none)>
2013-10-19 21:01:48 +0400

    addon default preferences

---
+++ FF-addon/defaults/preferences/defaults.js
@@ -0,0 +1,14 @@
+ pref("extensions.lspnr.default_escrow", "myescrow");
+ pref("extensions.lspnr.escrow_list", "myescrow");
+ pref("extensions.lspnr.snapshot_id", "mysnap-id");
+
+
+ pref("extensions.lspnr.escrow_myescrow.dnsname", "some.url");
+ pref("extensions.lspnr.escrow_myescrow.getuserurl", "ano.ther.url");
+ pref("extensions.lspnr.escrow_myescrow.listmetricsurl", "url.url");
+ pref("extensions.lspnr.escrow_myescrow.describeinstancesurl", "url.url");
+ pref("extensions.lspnr.escrow_myescrow.describevolumesurl", "url.url");
+ pref("extensions.lspnr.escrow_myescrow.getconsoleoutputurl", "url.url");
+
+
+
